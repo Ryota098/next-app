d3358a4d867cd0f29c7edfb0693f65ff39da17fc
Ryota Kuba <kubaryota@yahoo.co.jp>
2025-11-24 14:47:23 +0900

    logを出すようした

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
 'use client'
 
 import { Flex, Text } from '@chakra-ui/react'
+import { useEffect } from 'react'
 
 export default function HomePage() {
+  useEffect(() => {
+    // 初回マウント時
+    console.log('ページが表示されました')
+
+    // タブ復帰時
+    const handleVisibility = () => {
+      if (document.visibilityState === 'visible') {
+        console.log('ハイサイ！')
+      }
+    }
+
+    document.addEventListener('visibilitychange', handleVisibility)
+    return () => document.removeEventListener('visibilitychange', handleVisibility)
+  }, [])
+
   return (
     <Flex
       justifyContent='center'
