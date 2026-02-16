a9e91b5333324b14f94e179d580adf6e31514b00
Ryota Kuba <kubaryota@yahoo.co.jp>
2026-02-16 13:54:55 +0900

    123

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function HomePage() {
         fontWeight='bold'
         color='gray.800'
       >
-        Develop ブランチ
+        Hello World from Dev ブランチ
       </Text>
     </Flex>
   )
